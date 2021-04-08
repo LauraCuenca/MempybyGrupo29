8f40408acc1f516543934bbd261b7e547d9c851a
Diego Lanciotti <aaadiego502@gmail.com>
2021-04-08 17:35:49 -0300

    Corrige filtros

--- Actividad1-b.py
+++ Actividad1-b.py
@@ -1,21 +1,23 @@
-alumnos = [("Mauro", 5, 2, 13), ("Diego", 2, 7, 9), ("Laura", 10, 9, 19)]
+alumnos = [("Diego", 2, 7, 9), ("Mauro", 5, 2, 13), ("Laura", 10, 9, 19)]
 
-def reportes(alumnos, opcion):
+
+
+def reportes(alumnos, opcion, minimo, maximo):
     """ Imprime una tupla de los alumnos que cumplen las condiciones
         mínimas y máximas
 
         opcion == 1: ingresa por eval1
         opcion == 2: ingresa por eval2
         opcion == 3: ingresa por suma
+
+        return: lista de tuplas de los alumnos que estan en el rango
     """
 
-    min = 0
-    max = 9
-
-    def reporteEval1(alumnos,min,max):
-        nota1 = alumnos[0][1]
-        if (nota1 >= min) and (nota1 <= max):
-            return nota1
+    
+    def reporteEval1(alumnos, minimo, maximo, i, alumnos_filtrados):
+        nota1 = alumnos[i][1]
+        if (nota1 >= minimo) and (nota1 <= maximo):
+            alumnos_filtrados.append(alumnos[i])
 
     def reporteEval2(alumnos,min,max):
         nota2 = alumnos[0][2]
@@ -27,18 +29,26 @@ def reportes(alumnos, opcion):
         if (suma >= min) and (suma <= max):
             return suma
 
+    
+    alumnos_filtrados = [] # Los alumnos que cumplen la condicion (lista auxiliar)
 
-    for alumno in alumnos:
+    
+    for i in range(len(alumnos)):
         if (opcion == 1):
-            reporteEval1(alumnos,min,max)
-            print(f"los estudiantes con valores dentro del rango son {alumnos[0][0]}")
-        
+            reporteEval1(alumnos, minimo, maximo, i, alumnos_filtrados)
+            #print(f"los estudiantes con valores dentro del rango son {alumnos[0][0]}")
         elif (opcion == 2):
             reporteEval2(alumnos,min,max)
             print(f"los estudiantes con valores dentro del rango son {alumnos[1][0]}")
         else:
             reporteSuma_Notas(alumnos,min,max)
             print(f"los estudiantes con valores dentro del rango son {alumnos[2][0]}")
+    print(alumnos_filtrados)
 
 
-print(reportes(alumnos,3))    
+
+
+minimo = 3#input()
+maximo = 7#input()
+
+reportes(alumnos, 1, minimo, maximo)   
