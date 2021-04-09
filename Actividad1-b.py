f21751cdd9bd75e5b0c5b79ac451632f5ac8f2e3
Mauro Aisit <mauro-rb@hotmail.com>
2021-04-09 02:11:08 -0300

    Modifique el reporte

--- Actividad1-b.py
+++ Actividad1-b.py
@@ -1,7 +1,6 @@
 alumnos = [("Diego", 2, 7, 9), ("Mauro", 5, 2, 13), ("Laura", 10, 9, 19)]
 
 
-
 def reportes(alumnos, opcion, minimo, maximo):
     """ Imprime una tupla de los alumnos que cumplen las condiciones
         mínimas y máximas
@@ -14,20 +13,20 @@ def reportes(alumnos, opcion, minimo, maximo):
     """
 
     
-    def reporteEval1(alumnos, minimo, maximo, i, alumnos_filtrados):
+    def reporteEval1(alumnos,minimo,maximo,i,alumnos_filtrados):
         nota1 = alumnos[i][1]
         if (nota1 >= minimo) and (nota1 <= maximo):
             alumnos_filtrados.append(alumnos[i])
 
-    def reporteEval2(alumnos,min,max):
-        nota2 = alumnos[0][2]
-        if (nota2 >= min) and (nota2 <= max):
-            return nota2
+    def reporteEval2(alumnos,minimo,maximo,i,alumnos_filtrados):
+        nota2 = alumnos[i][2]
+        if (nota2 >= minimo) and (nota2 <= maximo):
+            alumnos_filtrados.append(alumnos[i])
 
-    def reporteSuma_Notas(alumnos,min,max):
-        suma = alumnos[0][3]
-        if (suma >= min) and (suma <= max):
-            return suma
+    def reporteSuma_Notas(alumnos,minimo,maximo,i,alumnos_filtrados):
+        suma = alumnos[i][3]
+        if (suma >= minimo) and (suma <= maximo):
+            alumnos_filtrados.append(alumnos[i])
 
     
     alumnos_filtrados = [] # Los alumnos que cumplen la condicion (lista auxiliar)
@@ -35,20 +34,16 @@ def reportes(alumnos, opcion, minimo, maximo):
     
     for i in range(len(alumnos)):
         if (opcion == 1):
-            reporteEval1(alumnos, minimo, maximo, i, alumnos_filtrados)
-            #print(f"los estudiantes con valores dentro del rango son {alumnos[0][0]}")
+            reporteEval1(alumnos,minimo,maximo,i,alumnos_filtrados)
         elif (opcion == 2):
-            reporteEval2(alumnos,min,max)
-            print(f"los estudiantes con valores dentro del rango son {alumnos[1][0]}")
+            reporteEval2(alumnos,minimo,maximo,i,alumnos_filtrados)
         else:
-            reporteSuma_Notas(alumnos,min,max)
-            print(f"los estudiantes con valores dentro del rango son {alumnos[2][0]}")
+            reporteSuma_Notas(alumnos,minimo,maximo,i,alumnos_filtrados)
+            
     print(alumnos_filtrados)
 
 
+minimo = int(input("Ingrese un valor mínimo: "))
+maximo = int(input("Ingrese un valor máximo: "))
 
-
-minimo = 3#input()
-maximo = 7#input()
-
-reportes(alumnos, 1, minimo, maximo)   
+reportes(alumnos,1,minimo,maximo)   
