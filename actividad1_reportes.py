c0b39a814b540322c91ee28941f7c0495cda90f5
Diego Lanciotti <aaadiego502@gmail.com>
2021-04-10 10:51:40 -0300

    Corrige error en reportes

--- actividad1_reportes.py
+++ actividad1_reportes.py
@@ -12,27 +12,27 @@ def reportes(alumnos, opcion, minimo, maximo):
 
     def reporteEval1(alumno, minimo, maximo, alumnos_filtrados):
         if (minimo <= alumno[1] <= maximo):
-            alumnos_filtrados.append(alumno[1])
+            alumnos_filtrados.append(alumno)
 
     def reporteEval2(alumno, minimo, maximo, alumnos_filtrados):
         if (minimo <= alumno[2] <= maximo):
-            alumnos_filtrados.append(alumno[2])
+            alumnos_filtrados.append(alumno)
 
     def reporteSuma_Notas(alumno, minimo, maximo, alumnos_filtrados):
         if (minimo <= alumno[3] <= maximo):
-            alumnos_filtrados.append(alumno[3])
+            alumnos_filtrados.append(alumno)
 
     
     alumnos_filtrados = [] # Los alumnos que cumplen la condicion (lista auxiliar)
 
     
-    for i in range(len(alumnos)):
+    for alumno in alumnos:
         if (opcion == "1"):
-            reporteEval1(alumnos[i], minimo, maximo, alumnos_filtrados)
+            reporteEval1(alumno, minimo, maximo, alumnos_filtrados)
         elif (opcion == "2"):
-            reporteEval2(alumnos[i], minimo, maximo, alumnos_filtrados)
+            reporteEval2(alumno, minimo, maximo, alumnos_filtrados)
         else:
-            reporteSuma_Notas(alumnos[i], minimo, maximo, alumnos_filtrados)
+            reporteSuma_Notas(alumno, minimo, maximo, alumnos_filtrados)
             
     return alumnos_filtrados
 
