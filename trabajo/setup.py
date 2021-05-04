3a9b5c2c17eef9dec5bac17b25b29f55f142dda0
Mauro Aisit <mauro-rb@hotmail.com>
2021-05-04 18:30:24 -0300

    Agrego la información del setup:

---
+++ trabajo/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='MemPy',
+    version='1.0',
+    description='Ejemplo de juego MemPy con PySimpleGui del Grupo29 de alumnos de la Facultad de Informática UNLP',
+    authors='Mauro Aisit', 'Laura Cuenca', 'Diego Lanciotti'
+    authors_emails='mauro-rb@hotmail.com', '', ''
+    url='https://gitlab.catedras.linti.unlp.edu.ar/python/entregas/grupo29/-/tree/master/trabajo',
+    packages=['UNLP', 'Python']
+)
