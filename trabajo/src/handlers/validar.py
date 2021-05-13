80e8bef08d6cbe56acee88f76db84cfb407f2ea0
Diego Lanciotti <aaadiego502@gmail.com>
2021-05-12 21:31:24 -0300

    Mejora handler registro

--- trabajo/src/handlers/validar.py
+++ trabajo/src/handlers/validar.py
@@ -7,26 +7,16 @@ import os
 def archivo_existe(jugador):
     """Verifica si el archivo exite, en caso contrario lo crea"""
     try:
-        with open("jugadores", 'rb+') as archivo:
-            guardar_reg(jugador)   
+        with open("jugadores", 'rb') as archivo:
+            lista_jugador= pickle.load(archivo)
+            print(lista_jugador)
     except FileNotFoundError as e:
-        arch= open("jugadores", 'xb')
-        cargar_jug(jugador)
-
-
-def cargar_jug(lista_jugador):
-    """ Carga el nuevo jugador"""
-    with open("jugadores",'wb') as arch:
-        pickle.dump(lista_jugador, arch)
-
-
-def guardar_reg(jugador):
-    """ Guarda el jugador en el archivo"""
-    with open("jugadores", 'rb') as archivo:
-        lista_jugador= pickle.load(archivo)
-        print(lista_jugador)
+        lista_jugador = []
+    finally:
         lista_jugador.append(jugador)
-        cargar_jug(lista_jugador)
+        with open("jugadores", 'wb') as archivo:
+            pickle.dump(lista_jugador, archivo)
+
 
 
 def iniciar_sesion(usuario,cont,conf,genero,edad):
