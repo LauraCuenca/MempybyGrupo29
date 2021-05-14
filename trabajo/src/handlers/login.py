203541bb3ff2c2ffccba8c85ba66c1793a55fa15
Laura <laura.cuenca1@gmail.com>
2021-05-14 22:25:42 +0200

    Cambiando funcion del login

--- trabajo/src/handlers/login.py
+++ trabajo/src/handlers/login.py
@@ -7,8 +7,13 @@ def comparacion(usuario,cont):
     try:
         with open("jugadores",'rb') as archivo:
             jugadores= pickle.load(archivo)
-            if usuario in jugadores and jugadores[0][1]== cont:
-                return True
+            jugadores_dict = {}
+            for jugador in jugadores:
+                for usuario, v in jugador.items():
+                     jugadores_dict[k] = v
+            
+            if (usuario in jugadores_dict) and jugadores_dict[usuario][1]== cont:
+                    return True
             else:
                 sg.SystemTray.notify('Error!', 'Debe registrarse', icon="src/recursos/images/exclamation.png")
     except FileNotFoundError as e:
