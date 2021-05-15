830f29f6f92eb441d2a2146d2783a4c309388c2b
Diego Lanciotti <aaadiego502@gmail.com>
2021-05-15 19:27:10 -0300

    Precarga pantalla de configuracion

--- trabajo/src/windows/configuracion.py
+++ trabajo/src/windows/configuracion.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+from src.handlers import login, configuracion_h
+
 
 def build():
     """ Crea la ventana cuando de configuracion"""
@@ -9,15 +11,17 @@ def build():
     size= (25,2)
     padding= (5, 5), (25, 5)
 
+    jugador_logueado = login.leer_sesion()
+    configuraciones = configuracion_h.leer_config()
+
     layout = [
         [sg.Text(" "*30),sg.Image(filename=("src/recursos/images/configuracion.png"),size=(150,150))],
-        [sg.Text("Nivel de Dificultad:", size=(size), pad=(padding)), sg.Combo(dificultad, default_value=dificultad[0], readonly=True, key="-NIVEL_DE_DIFICULTAD-")],
-        #[sg.Text("Ayuda:", size=(size), pad=(padding)), sg.Radio("Con ayuda", group_id=1, key="-AYUDA1-"), sg.Radio("Sin ayuda", group_id=1, default=True, key="-AYUDA2-")],
-        [sg.Text("Ayuda:", size=(size), pad=(padding)), sg.Combo(ayuda, default_value=ayuda[0], readonly=True, key="-AYUDA-")],
-        [sg.Text("Tipo de tarjetas:", size=(size)), sg.Combo(tipo_de_tarjetas, default_value=tipo_de_tarjetas[0], readonly=True, key="-TIPO_DE_TARJETAS-")],
-        [sg.Text("Tiempo total de partida:",size=(size),pad=(padding)),sg.Spin(list(range(1000)),size=(8,1),key="-TIEMPO_TOTAL_PARTIDA-")],
-        [sg.Text("Tema colores:", size=(size)), sg.Combo(tema, default_value=tema[0], readonly=True, key="-TEMA_COLOR-")],
-        [sg.Text("Mensajes de alerta:"), sg.Input(key="-MENSAJES_ALERTA-")],
+        [sg.Text("Nivel de Dificultad:", size=(size), pad=(padding)), sg.Combo(dificultad, default_value=configuraciones[jugador_logueado][0], readonly=True, key="-NIVEL_DE_DIFICULTAD-")],
+        [sg.Text("Ayuda:", size=(size), pad=(padding)), sg.Combo(ayuda, default_value=configuraciones[jugador_logueado][1], readonly=True, key="-AYUDA-")],
+        [sg.Text("Tipo de tarjetas:", size=(size)), sg.Combo(tipo_de_tarjetas, default_value=configuraciones[jugador_logueado][2], readonly=True, key="-TIPO_DE_TARJETAS-")],
+        [sg.Text("Tiempo total de partida:",size=(size),pad=(padding)),sg.Spin(list(range(1000)),size=(8,1),key="-TIEMPO_TOTAL_PARTIDA-", initial_value=configuraciones[jugador_logueado][3])],
+        [sg.Text("Tema colores:", size=(size)), sg.Combo(tema, default_value=configuraciones[jugador_logueado][4], readonly=True, key="-TEMA_COLOR-")],
+        [sg.Text("Mensajes de alerta:"), sg.Input(key="-MENSAJES_ALERTA-", default_text=configuraciones[jugador_logueado][5])],
         [sg.Text(" "*30), sg.Button("Guardar cambios", size=(20,2), key=("-GUARDAR_CAMBIOS-"))]
     ]
 
