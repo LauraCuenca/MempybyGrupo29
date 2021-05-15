4b6e16a39323dd532c1c7da700bf3d62d447e53f
Diego Lanciotti <aaadiego502@gmail.com>
2021-05-15 19:57:42 -0300

    Verifica si el usuario ya existe

--- trabajo/src/handlers/login.py
+++ trabajo/src/handlers/login.py
@@ -7,15 +7,9 @@ def comparacion(usuario,cont):
     """ Valida los datos para iniciar sesion"""
     try:
         with open("jugadores",'rb') as archivo:
-            jugadores= pickle.load(archivo)
-            jugadores_dict = {}
-            for jugador in jugadores:
-                for k, v in jugador.items():
-                     jugadores_dict[k] = v
-            
-            if (usuario in jugadores_dict) and jugadores_dict[usuario][1]== cont:
-                #guarda la sesion del jugador
-                crear_sesion(usuario)
+            jugadores= pickle.load(archivo)            
+            if (usuario in jugadores) and jugadores[usuario][1]== cont:
+                crear_sesion(usuario)  # guarda la sesion del jugador
                 return True
             else:
                 sg.SystemTray.notify('Error!', 'Debe registrarse', icon="src/recursos/images/exclamation.png")
--- trabajo/src/handlers/registrar_h.py
+++ trabajo/src/handlers/registrar_h.py
@@ -4,29 +4,42 @@ from src.handlers import configuracion_h
 
 
 def archivo_existe(jugador):
-    """Verifica si el archivo exite, en caso contrario lo crea"""
-    jugador = {jugador[0]: jugador}
+    """
+    Verifica si el archivo exite, en caso contrario lo crea
+    {'diego': ['diego', '123', 'm', '30'], 'mauro': ['mauro', '123', 'm', '32']}
+    """
     try:
         with open("jugadores", 'rb') as archivo:
             lista_jugador= pickle.load(archivo)
-            #print(lista_jugador)
     except FileNotFoundError as e:
-        lista_jugador = []
+        lista_jugador = {}
     finally:
-        lista_jugador.append(jugador)
+        lista_jugador[jugador[0]] = jugador
         with open("jugadores", 'wb') as archivo:
             pickle.dump(lista_jugador, archivo)
 
+
+def jugador_existe(jugador):
+    """Verifica si el usuario ya existe"""
+    try:
+        with open("jugadores", 'rb') as archivo:
+            lista_jugador= pickle.load(archivo)
+        print(lista_jugador)
+        return (jugador in lista_jugador)
+    except FileNotFoundError as e:
+        return False
 
 
 def validar(usuario,cont,conf,genero,edad):
     """ Valida que los campos del registro se llenen correctamente"""
     edad= str(edad)
-    #print(usuario,cont,conf,genero,edad)
     if "" in (usuario,cont,conf,genero,edad):
         sg.SystemTray.notify('Error!', 'Debes completar todos los campos', icon="src/recursos/images/exclamation.png")
         return False
     else:
+        if (jugador_existe(usuario)):
+            sg.SystemTray.notify('Error!', 'Ese usuario ya existe', icon="src/recursos/images/exclamation.png")
+            return False
         if((edad.isdigit()) and (genero.isalpha()) and cont==conf):
             jugador= [usuario,cont,genero,edad]
             archivo_existe(jugador)
