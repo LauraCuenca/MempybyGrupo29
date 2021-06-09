118cc1eecb1bb67d3d9ec058713957d805c0cdce
Diego Lanciotti <aaadiego502@gmail.com>
2021-06-09 17:07:12 -0300

    Agrega init a la clase juego

--- trabajo/src/handlers/juego.py
+++ trabajo/src/handlers/juego.py
@@ -1,22 +1,49 @@
-class Juego():
-    def __init__(self):
-        pass
+import datetime
+from src.handlers import tablero
+
+
+class Juego:
+    def __init__(self, dificultad, con_imagen, con_texto, jugador_nombre, jugador_genero, jugador_edad, nro_de_partida):
+        self.dificultad = dificultad
+        self.con_imagen = con_imagen
+        self.con_texto = con_texto
+        self.criterios = tablero.get_tabla_criterios()
+        self.jugador_nombre = jugador_nombre
+        self.jugador_genero = jugador_genero
+        self.jugador_edad = jugador_edad
+        self.nro_de_partida = nro_de_partida
+        self.matriz_tablero = []
 
     def generar_tablero(self):
         """
-        Verifica la hora, el dia, la dificultad y genera el tablero para jugar
+        Verifica la hora, el dia, la dificultad y genera la matriz para el tablero del juego
         """
-        pass
+        hoy = datetime.datetime.today()
+        dia_de_la_semana = hoy.weekday()
+        hora_del_dia = hoy.hour
+        if self.dificultad == "Fácil":
+            pass
+        elif self.dificultad == "Medio":
+            pass
+        else:
+            pass
+
+    def generar_matriz(self, x, y, datos):
+        """
+        Devuelve una matriz para ser usada como tablero
+        """
+        matriz = []
+        return matriz
 
     def hay_coincidencia(self):
         """
-        Verifica que si hay coincidencia entre 2 tarjetas
+        Devuelve que si hay coincidencia entre 2 tarjetas
         """
         pass
 
     def hay_fin_del_juego(self):
         """
-        Verifica si ya alcanzaron todas las coincidencias o se alcanzo el tiempo limite
+        Devuelve si ya alcanzaron todas las coincidencias o se alcanzo el tiempo limite
         """
         pass
 
@@ -34,7 +61,10 @@ class Juego():
 
     def guardar_datos(self):
         """
-        Guarda los datos que se producen durante la partida
+        Guarda los datos de las acciones que se producen durante la partida
         """
         pass
 
+
+if __name__ == '__main__':
+    pass
