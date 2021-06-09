05a653e0a10f379fc8f9817185f2825d52396c20
Diego Lanciotti <aaadiego502@gmail.com>
2021-06-09 16:00:08 -0300

    Agrega clase base del juego

---
+++ trabajo/src/handlers/juego.py
@@ -0,0 +1,40 @@
+class Juego():
+    def __init__(self):
+        pass
+
+    def generar_tablero(self):
+        """
+        Verifica la hora, el dia, la dificultad y genera el tablero para jugar
+        """
+        pass
+
+    def hay_coincidencia(self):
+        """
+        Verifica que si hay coincidencia entre 2 tarjetas
+        """
+        pass
+
+    def hay_fin_del_juego(self):
+        """
+        Verifica si ya alcanzaron todas las coincidencias o se alcanzo el tiempo limite
+        """
+        pass
+
+    def revelar_tarjeta(self):
+        """
+        Muestra el resultado de la tarjeta al voltearla
+        """
+        pass
+
+    def esconder_tarjeta(self):
+        """
+        Esconde una tarjeta revelada
+        """
+        pass
+
+    def guardar_datos(self):
+        """
+        Guarda los datos que se producen durante la partida
+        """
+        pass
+
