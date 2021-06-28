72d54f5af367e709d8201c9c168ec3bbe1a74b09
Laura <laura.cuenca1@gmail.com>
2021-06-28 19:24:01 +0200

    Arreglo de ventana de acerca_de

--- trabajo/src/windows/acerca_de.py
+++ trabajo/src/windows/acerca_de.py
@@ -5,12 +5,12 @@ import PySimpleGUI as sg
 def build():
     """ Crea la ventana para registrar el usuario"""
     size= (20,2)
-    size2= (30,15)
+    size2= (15,15)
 
     layout = [
     [sg.Image(filename=("src/recursos/images/acerca_de.png"),size=(150,150))],   
-    [sg.Text("Mempy-By grupo29." 
-    "Este juego, se realizo entre 3 compañeros que realizan el Seminario de Lenguaje de Python, nuestros nombres son: Diego, Laura y Mauro."
+    [sg.Text("Mempy-By grupo29." ,size=(size))],
+    [sg.Text("Este juego, se realizo entre 3 compañeros que realizan el Seminario de Lenguaje de Python, nuestros nombres son: Diego, Laura y Mauro."
     "Tratamos de ponerle nuestra idea de juego, tantos en los gustos en los criterios, como en el diseño del juego.",size=(size2))],
     [sg.Button("Salir",key=("-SALIR-"),size=(size))]
     ]
