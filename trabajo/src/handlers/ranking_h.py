38570d0395199275a6ea2d92a91d38f8e5a277f6
Diego Lanciotti <aaadiego502@gmail.com>
2021-06-28 17:01:27 -0300

    Corrige ranking top 10

--- trabajo/src/handlers/ranking_h.py
+++ trabajo/src/handlers/ranking_h.py
@@ -1,5 +1,4 @@
 import PySimpleGUI as sg
-import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 from src.handlers import sonido
@@ -7,44 +6,27 @@ from src.handlers import sonido
 
 def obtener_datos():
     datos_puntos = pd.read_csv("datos_de_puntos.csv")
-    filtro= datos_puntos[['nick','puntos']].head(7)
-    ranking= filtro.sort_values(by='puntos',ascending=False)
-    print(ranking)
-    return ranking
+    filtro = datos_puntos[['nick', 'puntos']].head(10)
+    ranking = filtro.sort_values(by='puntos', ascending=False)
+    return ranking.values
 
-def iterar(data):
-    """"""
-    lista_de_listas = []
-    for x in range(len(data)):
-        lista_de_listas += [[data[x]]]
-    return lista_de_listas
-
-
-fig, ax =plt.subplots(1,1)
-data= obtener_datos()
-data_act=iterar(data)
-column_labels= [['Nick'],['Puntaje']]
-ax.axis('tight')
-ax.axis('off')
-ax.table(cellText=data_act,colLabels=column_labels,loc="center")
-plt.title("Ranking de puntajes")
-plt.savefig("src/recursos/graficos/ranking.png")
 
 def start():
     """ Lanza la ejecución de la ventana del tablero """
     window = loop()
     window.close()
 
+
 def build():
     """ Crea la ventana para registrar el usuario"""
-    size= (15,2)
+    size = (15, 2)
 
     layout = [
-    [sg.Image(filename="src/recursos/graficos/ranking.png")],
-    [sg.Button("Salir",size=(size),key=("-SALIR-"))]
+        [sg.Image(filename="src/recursos/graficos/ranking.png")],
+        [sg.Button("Salir", size=size, key="-SALIR-")]
     ]
-    
-    ranking = sg.Window("Grafico",layout=layout,size=(550,550),element_justification='c', resizable=False, modal=True)
+
+    ranking = sg.Window("Grafico", layout=layout, element_justification='c', resizable=False, modal=True)
 
     return ranking
 
@@ -53,9 +35,18 @@ def loop():
     """Loop de la ventana del tablero que capta sus eventos"""
     window = build()
 
+    fig, ax = plt.subplots(1, 1)
+    data = obtener_datos()
+    column_labels = ['Posicion', 'Nick', 'Puntaje']
+    ax.axis('tight')
+    ax.axis('off')
+    ax.table(cellText=data, colLabels=column_labels, loc="center")
+    plt.title("Ranking de puntajes")
+    plt.savefig("src/recursos/graficos/ranking.png")
+
     while True:
         event, values = window.read()
-        if event in (sg.WINDOW_CLOSED,"-SALIR-"):
+        if event in (sg.WINDOW_CLOSED, "-SALIR-"):
             sonido.reproducir_sonido(1)
             break
     return window
